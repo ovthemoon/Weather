b7854f00c3fbe2a28cda2f130ac5168a2dbc8583
ksh7536 <143468776+ksh7536@users.noreply.github.com>
2023-09-25 18:15:33 +0900

    Player Animation walk(press w,a,s,d)
    Player Animation walk(press w,a,s,d)

---
+++ Assets/Animation/Player/AniController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniController : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("walk");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("walk");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("walk");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("walk");
+        }
+    }
+}
