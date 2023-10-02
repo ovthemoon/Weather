e1bcb9cc19e8bb927eb0fd9c17bbc022221f3a5c
ovthemoon <53703003+ovthemoon@users.noreply.github.com>
2023-10-02 13:56:22 +0900

    Map0
    put Map0

--- Assets/Scripts/MirrorLogic/TestEffectShooter.cs
+++ Assets/Scripts/MirrorLogic/TestEffectShooter.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class TestEffectShooter : MonoBehaviour
 {
-    public GameObject testEffect;
-    public Vector3 spawnVelocity = new Vector3(0, 0, 10);
-    Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject projectilePrefab; // 발사할 프로젝트 타입(물체)
+    public Transform spawnPoint;
+    public float shootForce = 10f; // 발사되는 힘
+
+    // Update는 프레임마다 한 번씩 호출됩니다
+    private void Update()
     {
-       rb= GetComponent<Rigidbody>();
+        if (Input.GetButtonDown("Fire1")) // Fire1은 기본적으로 마우스 왼쪽 버튼을 의미합니다.
+        {
+            Debug.Log("Shoot");
+            Shoot();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        GameObject projectile = Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation); // 현재 위치와 회전에서 프로젝트를 생성합니다.
+        Destroy(projectile,2f);
+        Rigidbody rb = projectile.GetComponent<Rigidbody>(); // Rigidbody 컴포넌트에 접근합니다.
+        
+        if (rb)
         {
-            Instantiate(testEffect,transform.position,transform.rotation);
-            rb.velocity=spawnVelocity;
+            rb.AddForce(transform.up * shootForce, ForceMode.Impulse); // forward vector를 사용하여 물체를 발사합니다.
         }
     }
 }
