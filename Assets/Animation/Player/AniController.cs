ae1dedef9f8914087f8984b2159a86c90928a764
ovthemoon <53703003+ovthemoon@users.noreply.github.com>
2023-10-03 17:14:55 +0900

    Making Map0 and Animation Work
    map0 제작과 애니메이션 작업 진행

--- Assets/Animation/Player/AniController.cs
+++ Assets/Animation/Player/AniController.cs
@@ -2,29 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//필수 컴포넌트
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class AniController : MonoBehaviour
 {
     private Rigidbody rb;
-    Animator animator;
+    private Animator animator;
+    private CharacterMove characterMove;
 
-    bool isWalking;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        characterMove=GetComponent<CharacterMove>();
+        rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        MoveStateCheck();
+        JumpStateCheck();
+        
+    }
+    void MoveStateCheck()
+    {
+        if (characterMove.GetMovingState())
         {
-            isWalking = true;
+            animator.SetBool("IsWalking", true);
+            animator.SetFloat("HorizontalSpeed", Mathf.Clamp(characterMove.GetDirectionVector().x, -1, 1));
+            animator.SetFloat("VerticalSpeed", Mathf.Clamp(characterMove.GetDirectionVector().z, -1, 1));
         }
         else
-            isWalking = false;
-        animator.SetBool("IsWalking", isWalking);
+            animator.SetBool("IsWalking", false);
     }
+    void JumpStateCheck()
+    {
+        
+        animator.SetBool("IsGround",characterMove.GetGroundState());
+        animator.SetFloat("VelocityY", rb.velocity.y);
+    }
+    
 }
