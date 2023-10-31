2575c8fbf038b98fb94588323cf6c64fd2dddcc3
ovthemoon <seungmoonchoi71@gmail.com>
2023-10-31 15:44:35 +0900

    Effect 추가 및 기능 구현
    거울 객체 복사 기능 구현,
    lowpoly effect
    도어 애니메이션 및
    물체 들기기능 구현

--- Assets/Scripts/Character/MirrorAnimationPlayer.cs
+++ Assets/Scripts/Character/MirrorAnimationPlayer.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 public class MirrorAnimationPlayer : MonoBehaviour
 {
     public TimelineManager manager;
-    public GameObject CloneCollider;
+    public CloneManager cloneManager;
     public string sceneName;
     public Image image; // 조정하려는 메터리얼
     public float duration = 2f; // 트위닝에 걸리는 시간
@@ -19,11 +19,12 @@ public class MirrorAnimationPlayer : MonoBehaviour
     private void Start()
     {
         material = image.material;
+        material.color = new Color(material.color.r, material.color.g, material.color.b, 0);
     }
 
     private void OnMouseDown()
     {
-        CloneCollider.SetActive(false);
+        cloneManager.DeleteAllClones();
         manager.PlayTimeline();
         manager.playableDirector.stopped += OnTimelineStopped;
     }
@@ -31,8 +32,10 @@ public class MirrorAnimationPlayer : MonoBehaviour
     {
         image.gameObject.SetActive(true);
         Camera.main.DOShakePosition(duration, 2, 10, 90, true);
-        DOTween.To(() => material.GetFloat("_Cutoff"), x => material.SetFloat("_Cutoff", x), targetAlphaClipValue, duration).OnComplete(LoadNextScene);
-       
+        material.color = new Color(material.color.r, material.color.g, material.color.b, 1);
+        DOTween.To(() => material.GetFloat("_Cutoff"), x => material.SetFloat("_Cutoff", x), targetAlphaClipValue, duration)
+              .OnComplete(() => {DOVirtual.DelayedCall(2f, LoadNextScene);});
+
     }
     void LoadNextScene()
     {
