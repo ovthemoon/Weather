090ff7ca8b799e005eba61254a83212e25d3089e
Sean1999 <coreiu0210@gmail.com>
2023-11-04 16:56:32 +0900

    초반부 회전,이동 버튼프레스 카메라 시점 오브젝트 추가
    전반적인 시나리오 작성 완료 배치는 게임로직 씬 내 구현 중 일단 대기 쭉쭉 나열하기면 될 듯
    > 추가 필요 사항은 크리쳐, 포탈 이동, 버튼 프레스에 오브젝트 이동 회전 붙이기, 거울아이템 획득
    이미 구현한 미러로직 다시 확인 및 붙이기

--- Assets/Scripts/GameLogic/PressButtonTest.cs
+++ Assets/Scripts/GameLogic/PressButtonTest.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PressButton : MonoBehaviour
+public class PressButtonTest : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject portal;
 
+    //Mesh변경 추가
+    [SerializeField] private Material activeMaterial; // 활성화될 때 적용할 Material
+
+
+    private Material originalMaterial; // 원래의 Material을 저장할 변수
+    private MeshRenderer portalRenderer; // 포털의 MeshRenderer 컴포넌트를 캐싱할 변수
    
     private void OnCollisionStay(Collision collision)
     {
@@ -16,6 +22,12 @@ public class PressButton : MonoBehaviour
         Debug.Log("��ư ����");
 
         portal.GetComponent<Collider>().isTrigger = true;
+
+        // 포털의 Material을 변경합니다.
+        if (portalRenderer != null)
+        {
+            portalRenderer.material = activeMaterial;
+        }
         
     }
 
@@ -27,6 +39,13 @@ public class PressButton : MonoBehaviour
         Debug.Log("��ư �ȴ���");
 
         portal.GetComponent<Collider>().isTrigger = false;
+
+
+        // 포털의 Material을 원래대로 돌립니다.
+        if (portalRenderer != null)
+        {
+            portalRenderer.material = originalMaterial;
+        }
     }
 
 
