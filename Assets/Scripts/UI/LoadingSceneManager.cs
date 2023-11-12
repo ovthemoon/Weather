311bcab01ea432e804a2a94aa7b40a40176504e5
killyouhee <97865497+killyouhee@users.noreply.github.com>
2023-11-12 19:59:22 +0900

    로딩씬 수정

--- Assets/Scripts/UI/LoadingSceneManager.cs
+++ Assets/Scripts/UI/LoadingSceneManager.cs
@@ -32,18 +32,47 @@ public class LoadingSceneManager : MonoBehaviour
         {
             yield return null;
             timer += Time.deltaTime;
-            if (op.progress < 0.9f)
+
+            /*
+            if (op.progress >= 0.9f)
             {
                 progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
+                
                 if (progressBar.fillAmount >= op.progress)
                 {
                     timer = 0f;
+                }
+                
+                
+            }
+            else
+            {
+                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
+                
+               
+                if (progressBar.fillAmount == 1.0f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+                
+            }
+            */
+
+            if(op.progress < 0.9f)
+            {
+                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
+
+                if(progressBar.fillAmount >= op.progress)
+                {
+                    timer = 0.0f;
                 }
             }
             else
             {
                 progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
-                if (progressBar.fillAmount == 1.0f)
+
+                if(progressBar.fillAmount == 1.0f)
                 {
                     op.allowSceneActivation = true;
                     yield break;
